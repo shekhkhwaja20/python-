d1e37a788a103ff94c43bea9d0be436162e986d1
shekh khwaja julaha <khwajajulaha122@gmail.com>
2026-01-23 13:18:38 +0530

    basics

---
+++ basics/avgoftwo.py
@@ -0,0 +1,3 @@
+a=int(input("Enter the value of a="))
+b=int(input("Enter the value of b="))
+print("Average=",(a+b)/2)
---
+++ basics/squarearea.py
@@ -0,0 +1,3 @@
+side=float(input("Enter the value of side of the square="))
+area=side*side
+print("Area of square is=",area100)
