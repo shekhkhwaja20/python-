727a82245443bb5acaf251ec87be584ce570a9e3
shekh khwaja julaha <khwajajulaha122@gmail.com>
2026-01-25 08:51:11 +0530

    2nd day

---
+++ String and Conditionals/concatenaton1.py
@@ -0,0 +1,8 @@
+str1="shekh"
+str2="julaha"
+print(str1+str2)
+
+#if we want to add blank space in our string
+str1="shekh"
+str2="julaha"
+print(str1+" "+str2) 
---
+++ String and Conditionals/escapechar.py
@@ -0,0 +1,3 @@
+str1="hello my name is shekh khwaja.\n We are creating in python"
+print(str1)
+#(\n)---------------> used for next line 
