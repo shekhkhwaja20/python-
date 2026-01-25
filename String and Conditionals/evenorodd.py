e2c8d042b33c7b4b96fa7b7d8608fe5f3962edc1
shekh khwaja julaha <khwajajulaha122@gmail.com>
2026-01-25 10:04:59 +0530

    today

---
+++ String and Conditionals/evenorodd.py
@@ -0,0 +1,5 @@
+a=int(input("Enter the value of a="))
+if(a%2==0):
+   print("Even no")
+else:
+   print("odd")
---
+++ String and Conditionals/multiplicationof7.py
@@ -0,0 +1,5 @@
+a=int(input("Enter the value of a="))
+if(a%7==0):
+  print("multiplication of 7")
+else:
+  print("not a mulitiplication of 7")
