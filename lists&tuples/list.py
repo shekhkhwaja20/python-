26c72bba77423c1342d9c83c736c5ed64135aa20
shekh khwaja julaha <khwajajulaha122@gmail.com>
2026-01-27 21:19:35 +0530

    list

---
+++ lists&tuples/list.py
@@ -0,0 +1,3 @@
+marks=[24,55,36,25,51,65]
+#it also uses indexing so we can access any value by the index..
+print(marks[0])
---
+++ lists&tuples/list1.py
@@ -0,0 +1,3 @@
+student=["karan",25,65.55]
+print(student)
+#it can store any type of value in it..
